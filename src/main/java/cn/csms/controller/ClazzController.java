0e13dfc399a8ef0153b97bdde514fbb16e016b1a
NaturePorter <879704116@qq.com>
2021-03-01 00:44:08 +0800

    考试列表-获取班级年级列表

--- src/main/java/cn/csms/controller/ClazzController.java
+++ src/main/java/cn/csms/controller/ClazzController.java
@@ -1,9 +1,23 @@
 package cn.csms.controller;
 
 
+import cn.csms.base.ResultBean;
+import cn.csms.base.ResultConst;
+import cn.csms.entity.Clazz;
+import cn.csms.entity.Grade;
+import cn.csms.service.ClazzService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +30,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/csms_api")
 public class ClazzController {
+
+    @Autowired
+    ClazzService clazzService;
+
+    @GetMapping("clazzs")
+    public ResponseEntity getGrade(int id){
+        LambdaQueryWrapper<Clazz> queryWrapper = Wrappers.<Clazz>lambdaQuery().eq(Clazz::getGradeid, id);
+        List<Clazz> clazzs = clazzService.list(queryWrapper);
+        return new ResponseEntity<>(ResultBean.ok(ResultConst.GET_GRADES_SUCC, clazzs), HttpStatus.OK);
+    }
 
 }
